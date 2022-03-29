5b893cea701e018f722509a697115f17560e966d
Volkan Müjdat Tiryaki <48734237+tiryakiv@users.noreply.github.com>
2022-03-29 22:58:26 +0300

    Update mar29_22.m

--- mar29_22.m
+++ mar29_22.m
@@ -1,21 +1,21 @@
 clear; close all; clc; format compact;
 
-%Sýcaklýk deðerleri Meteoroloji GM sitesinden alýndý 
+%Sicaklik degerleri Meteoroloji GM sitesinden alindi
 temp_mat=[2.7 4.3 8.4 13.8 19.5 26.0 30.7 30.4 25.5 18.3 10.5 4.8];
 temp_mat(2,:)=[-9.1 -7.7 -2.4 5.4 10.7 14.9 19.2 19.5 14.8 8.2 1.1 -5.8];
 temp_mat(3,:)=[0.2	1.7	5.7	11.2	16.1	20.0	23.4	23.4	18.9	13.2	7.3	2.5];
 aylar={"ocak","subat","mart","nisan","mayis","haziran","temmuz","agustos",...
 "eylul","ekim","kasim","aralik"};
 
-% Grafik çizimi
+% Grafik Ã§izimi
 hold on; 
 plot(temp_mat(1,:)); 
 plot(temp_mat(2,:));
 plot(temp_mat(3,:));
 set(gca,'xtick', 1:12,'xticklabel',aylar)
-title('Siirt, Erzurum, Çorum illeri yillik sicaklik grafigi');
+title('Siirt, Erzurum, Ã‡orum illeri yillik sicaklik grafigi');
 axis([1 12 -10 40]);
 xlabel('Aylar'); ylabel('\deg C');
-legend('Siirt','Erzurum','Çorum');
+legend('Siirt','Erzurum','Ã‡orum');
 hold off;
 grid on;
