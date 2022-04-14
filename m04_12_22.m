d56a84244c03ce83320651a5bab480e987712a55
Volkan Müjdat Tiryaki <48734237+tiryakiv@users.noreply.github.com>
2022-04-15 01:51:28 +0300

    Update m04_12_22.m

--- m04_12_22.m
+++ m04_12_22.m
@@ -1,37 +1,39 @@
-% Denklem sistemlerinin çözümü 
-% İki bilinmeyenli denklem çözümü
+% Denklem sistemlerinin Ã§Ã¶zÃ¼mÃ¼ 
+% Ä°ki bilinmeyenli denklem Ã§Ã¶zÃ¼mÃ¼
 % Denklemler
 %  2x+y=3
 %   x-y=5
-% Çözüm sonucu: x=8/3  y=-7/3
+% Ã‡Ã¶zÃ¼m sonucu: x=8/3  y=-7/3
 
 clear; clc; close all;
 
-A=[2 1;1 -1]; %katsayı matrisi
+A=[2 1;1 -1]; %katsayÄ± matrisi
 B=[3;5];  %sabit matrisi
 
 x=inv(A)*B   %  x=[2.6667;  -2.3333]
 
 clear
-% Üc bilinmeyenli denklem çözümü
+% Ãœc bilinmeyenli denklem Ã§Ã¶zÃ¼mÃ¼
 % Denklemler:
 % 2x+y-z=1
 % 3x-y+2z=-1
 % x+y-z=4
 
-A=[2 1 -1; 3 -1 2;1 1 -1];%katsayı matrisi
+A=[2 1 -1; 3 -1 2;1 1 -1];%katsayÄ± matrisi
 B=[1;-1;4]; %sabit matrisi
-%x=inv(A)*B  %çözüm matrisi
+%x=inv(A)*B  %Ã§Ã¶zÃ¼m matrisi
 
-x=A\B  %veya bu şekilde de çözülebilir 
+x=A\B  %veya bu ÅŸekilde de Ã§Ã¶zÃ¼lebilir 
 
 %% istatistiksel fonksiyonlar 
-O=[5 8 9 2 8 4 7 5 3 7 5] %örnek bir veri
+O=[5 8 9 2 8 4 7 5 3 7 5] %Ã¶rnek bir veri
 ort=mean(O)   %ortalama
 stsap=std(O)  %standard sapma
-mod1=mode(O)  %mod (en sık tekrar eden eleman)
+mod1=mode(O)  %mod (en sÄ±k tekrar eden eleman)
 med=median(O) %medyan 
-min1=min(O)   %en küçük değer
-max1=max(O)   %en büyük değer
+min1=min(O)   %en kÃ¼Ã§Ã¼k deÄŸer
+max1=max(O)   %en bÃ¼yÃ¼k deÄŸer
 var1=var(O)   %varyans
-hist(O)       %histogram grafiği 
+hist(O)       %histogram grafiÄŸi 
+
+
