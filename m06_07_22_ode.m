f5ceaf558e10e457f23c1f5b0743a0277454a167
Volkan Müjdat Tiryaki <48734237+tiryakiv@users.noreply.github.com>
2022-06-27 13:59:46 +0300

    Rename m06_07_22.m to m06_07_22_ode.m

--- m06_07_22_ode.m
+++ m06_07_22_ode.m
@@ -1,19 +1,19 @@
 % Ordinary differential equation (ode) solution with Octave
 % kaynak : https://www.youtube.com/watch?v=6_tvM9b4sLQ
 clear; clc; close all; format compact;
-tmin=0; tmax=3; #başlangıç ve bitiş zamanları
+tmin=0; tmax=3; #baÅŸlangÄ±Ã§ ve bitiÅŸ zamanlarÄ±
 
 tspan= [tmin tmax];
-y0=1; #başlangıç durumu (boundary conditions)
-#y0 su tankında su seviyesinin başlangıç değerini gösterir
-[t,y]=ode45(@(t,y)myode(t,y), tspan,y0); #octave ode çözümü
+y0=1; #baÅŸlangÄ±Ã§ durumu (boundary conditions)
+#y0 su tankÄ±nda su seviyesinin baÅŸlangÄ±Ã§ deÄŸerini gÃ¶sterir
+[t,y]=ode45(@(t,y)myode(t,y), tspan,y0); #octave ode Ã§Ã¶zÃ¼mÃ¼
 
 plot(t,y,'ro', 'Linewidth',1, 'MarkerSize',16);
 set(gca,'linewidth',1, 'fontsize',24);
 hold on
-# Manuel çözüm 
-t_exact=linspace(tmin, tmax); # zaman değişkeni
-y_exact=exp(-2*t_exact);      # Manuel çözüm 
+# Manuel Ã§Ã¶zÃ¼m 
+t_exact=linspace(tmin, tmax); # zaman deÄŸiÅŸkeni
+y_exact=exp(-2*t_exact);      # Manuel Ã§Ã¶zÃ¼m 
 plot(t_exact,y_exact,'b:', 'Linewidth',1, 'MarkerSize',16);
 set(gca,'linewidth',1, 'fontsize',24);
 grid on 
